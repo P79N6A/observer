5fb26f5a3bb9a5ff8ed33dc750edf2752eb1013b
wapu <wapu@qq.com>
2019-01-14 01:34:34 +0800

    no message

--- test/test.py
+++ test/test.py
@@ -479,9 +479,9 @@ def fenxi():
 
     jieba.add_word('海尔茂')
 
-    txt = open('Init.txt', encoding='utf-8').read().replace("\n", "").replace(' ', '')
+    txt = open('res/Init.txt', encoding='utf-8').read().replace("\n", "").replace(' ', '')
 
-    txt2 = [line.strip() for line in open('stoptxt.txt', encoding='utf-8').readlines()]
+    txt2 = [line.strip() for line in open('res/stoptxt.txt', encoding='utf-8').readlines()]
 
     words = jieba.lcut(txt)
     en = {}
@@ -497,7 +497,7 @@ def fenxi():
     for i in range(100):
         # word,count = items[i]
         print(items[i])
-    wc.generate(text)
+    wc.generate(txt)
 
 #分词测试
 def fdfdf():
@@ -580,13 +580,13 @@ def stopkey():
 def fenci():
     import jieba.analyse
     print("***案例1***" * 3)
-    txt = '那些你很冒险的梦，我陪你去疯，折纸飞机碰到雨天终究会坠落，伤人的话我直说，因为你会懂，冒险不冒险你不清楚，折纸飞机也不会回来，做梦的人睡不醒！'
+    txt = open('res/Init.txt', encoding='utf-8').read().replace("\n", "").replace(' ', '')
     Key = jieba.analyse.extract_tags(txt, topK=3)
     print(Key)
     # -----------------------------------------------------------------------------------
     print("***案例2***" * 3)
     # 字符串前面加u表示使用unicode编码
-    content = u'中国特色社会主义是我们党领导的伟大事业，全面推进党的建设新的伟大工程，是这一伟大事业取得胜利的关键所在。党坚强有力，事业才能兴旺发达，国家才能繁荣稳定，人民才能幸福安康。党的十八大以来，我们党坚持党要管党、从严治党，凝心聚力、直击积弊、扶正祛邪，党的建设开创新局面，党风政风呈现新气象。习近平总书记围绕从严管党治党提出一系列新的重要思想，为全面推进党的建设新的伟大工程进一步指明了方向。'
+    content = open('res/Init.txt', encoding='utf-8').read().replace("\n", "").replace(' ', '')
     # 第一个参数：待提取关键词的文本
     # 第二个参数：返回关键词的数量，重要性从高到低排序
     # 第三个参数：是否同时返回每个关键词的权重
@@ -1992,4 +1992,4 @@ def to_url():
             print(i)
 
 if __name__ == '__main__':
-    yide()
+    fenci()
